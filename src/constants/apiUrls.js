7132a8ff8d8e75c0ec49ff2849693ec08058748d
Savelyi <savasvirsky@gmail.com>
2023-01-20 12:52:19 +0300

    Deleting Auth

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -3,6 +3,5 @@ export const apiDailyWeatherUrl =
 
 export const apiGeneralWeatherUrl =
     'https://api.weatherbit.io/v2.0/forecast/daily';
-export const apiLatAndLonByCityName =
-    'https://api.openweathermap.org/geo/1.0/direct';
+
 export const apiAbstractUrl = 'https://ipgeolocation.abstractapi.com/v1/';
