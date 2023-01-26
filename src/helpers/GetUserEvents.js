6cdf91bf0adb816ec971ffa5a216aa47c5be8fe1
Savelyi <savasvirsky@gmail.com>
2023-01-27 00:01:19 +0300

    adding maxResult to googleEvents

--- src/helpers/GetUserEvents.js
+++ src/helpers/GetUserEvents.js
@@ -4,22 +4,18 @@ import { googleApiGetEventsUrl } from '../constants/apiUrls';
 function GetUserEvents(token) {
     const now = new Date();
     return axios
-        .get(
-            googleApiGetEventsUrl,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                params: {
-                    timeMin: now.toISOString(),
-                    timeMax: new Date(
-                        now.setDate(now.getDate() + 3)
-                    ).toISOString(),
-                    singleEvents: 'true',
-                    orderBy: 'startTime',
-                },
-            }
-        )
+        .get(googleApiGetEventsUrl, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                timeMin: now.toISOString(),
+                timeMax: new Date(now.setDate(now.getDate() + 3)).toISOString(),
+                maxResults: 3,
+                singleEvents: 'true',
+                orderBy: 'startTime',
+            },
+        })
         .then((res) => res.data);
 }
 
